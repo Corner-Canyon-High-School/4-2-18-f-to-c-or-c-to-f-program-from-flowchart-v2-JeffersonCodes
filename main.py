0b35f1c2f5c1495adca261143453a0db71c41b22
JeffersonCodes <162729933+JeffersonCodes@users.noreply.github.com>
2024-04-17 16:02:59 +0000

    SUBMIT

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 
+temp = int(input("Enter the temperature: "))
+type = int(input("Type 1 for Fahrenheit-to-Centigrade. Type 2 for Centigrade-to-Fahrenheit "))
+if type == 2:
+    C = ((5*(temp-32))/9)
+    print(C, "degrees Centigrade")
+if type == 1:
+    F = (9*temp + (32 * 5))/5
+    print(F, "degrees Fahrenheit")
